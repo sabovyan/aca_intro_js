8edb5577e5bc4776e91be5b05e4f338abdc871c6
sabovyan <abovyan.sargis90@gmail.com>
2024-08-22 17:21:13 +0400

    add eslint jsx-i11y

--- app/components/H1.tsx
+++ app/components/H1.tsx
@@ -1,10 +1,12 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 
-export function H1(
-  props: DetailedHTMLProps<
-    HTMLAttributes<HTMLHeadingElement>,
-    HTMLHeadingElement
-  >,
-) {
-  return <h1 {...props} className="text-5xl font-bold my-16" />;
+export function H1({
+  children,
+  ...props
+}: DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) {
+  return (
+    <h1 {...props} className="text-5xl font-bold my-16">
+      {children}
+    </h1>
+  );
 }
